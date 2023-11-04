9ad1a8e5a06606df845cea328744be9644b176c3
miryrusso <russo.miriana@studium.unict.it>
2023-11-04 23:43:27 +0100

    Tiro alla fune

--- Thread/Tiro_alla_fune/app.java
+++ Thread/Tiro_alla_fune/app.java
@@ -1,5 +1,5 @@
 /**
- * Scrivere un programma in C che simuli un incontro di “tiro alla fune” tra due 2 thread “giocatori” tp[0], tp[1].
+ * Scrivere un programma in Java che simuli un incontro di “tiro alla fune” tra due 2 thread “giocatori” tp[0], tp[1].
 
     E’ data una variabile globale intera posizione (con valore iniziale 0) condivisa da tutti i thread. 
     Sono date inoltre due variabili globali intere, vittorie_tp0 e vittorie_tp1.
@@ -35,6 +35,68 @@
  */
 package Tiro_alla_fune;
 
+import java.util.Random;
+
 public class app {
-    
+    public static int posizione = 0;
+    public static int vittorie_tp0 = 0;
+    public static int vittorie_tp1 = 0;
+
+    public static void main(String[] args) {
+        Thread[] tp = new Thread[2];
+
+        tp[0] = new Thread(() -> {
+            while (vittorie_tp0 < 10 && vittorie_tp1 < 10) {
+                int recupero = new Random().nextInt(4);
+                int forza = new Random().nextInt(6);
+
+                try {
+                    Thread.sleep(recupero * 1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+
+                if (posizione >= 10) {
+                    vittorie_tp1++;
+                    posizione = 0;
+                    System.out.println("tp[0] ha vinto! Vittorie: " + vittorie_tp0 + " - " + vittorie_tp1);
+                } else {
+                    posizione -= forza;
+                    if (posizione <= -10) {
+                        System.out.println("tp[0] ha vinto! Vittorie: " + vittorie_tp0 + " - " + vittorie_tp1);
+                        break;
+                    }
+                }
+            }
+        });
+
+        tp[1] = new Thread(() -> {
+            while (vittorie_tp0 < 10 && vittorie_tp1 < 10) {
+                int recupero = new Random().nextInt(4);
+                int forza = new Random().nextInt(6);
+
+                try {
+                    Thread.sleep(recupero * 1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+
+                if (posizione <= -10) {
+                    vittorie_tp0++;
+                    posizione = 0;
+                    System.out.println("tp[1] ha vinto! Vittorie: " + vittorie_tp0 + " - " + vittorie_tp1);
+                } else {
+                    posizione += forza;
+                    if (posizione >= 10) {
+                        System.out.println("tp[1] ha vinto! Vittorie: " + vittorie_tp0 + " - " + vittorie_tp1);
+                        break;
+                    }
+                }
+            }
+        });
+
+        tp[0].start();
+        tp[1].start();
+    }
 }
+
